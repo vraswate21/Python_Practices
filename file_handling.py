12d61823a023902752780f8dc58941c9c7316bb0
vraswate21 <vikasraswate77@gmail.com>
2025-04-24 15:52:32 +0530

    added

---
+++ file_handling.py
@@ -0,0 +1,11 @@
+# Opening a file in read mode
+file = open('myfile.txt', 'r')
+
+# Opening a file in write mode
+file = open('myfile.txt', 'w')
+
+# Opening a file in append mode
+file = open('myfile.txt', 'a')
+
+# Always close the file after you're done with it
+file.close()
---
+++ greaternumber.py
@@ -0,0 +1,11 @@
+
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+c = int(input("enter third number"))
+
+if (a>=b and a>=c):
+    print("first number is largest",a)
+elif(b>=c):
+    print("second number is largest", b)
+else:
+    print("third number is largest", c)

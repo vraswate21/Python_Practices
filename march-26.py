781a18df502af32fafdaf2b8c25df4f7e29891b1
vraswate21 <vikasraswate77@gmail.com>
2025-04-26 11:02:20 +0530

    updated logic error

--- march-26.py
+++ march-26.py
@@ -3,8 +3,8 @@
 for num in range(100, 200):
     if num > 1:
         for i in range(2, int(num**0.5)+1):
-            if num % 1 ==0:
+            if num % i ==0:
                 break
             else:
                 print(num)
-        
+

fb345854995406c1817687705906d53138dd6f90
vraswate21 <vikasraswate77@gmail.com>
2025-04-26 09:18:21 +0530

    added

---
+++ march-26.py
@@ -0,0 +1,10 @@
+
+
+for num in range(100, 200):
+    if num > 1:
+        for i in range(2, int(num**0.5)+1):
+            if num % 1 ==0:
+                break
+            else:
+                print(num)
+        

aa6cf359361744b25841f2f950e788e2fe2c8b4c
vraswate21 <vikasraswate77@gmail.com>
2025-04-27 12:23:08 +0530

    added

---
+++ functions.py
@@ -0,0 +1,39 @@
+'''def greet():
+    print("hello world")
+
+# call the function
+greet()
+
+print("outside the function")
+'''
+
+'''def greet(name):
+    print("hello", name)
+
+
+
+#pass argument
+greet("john")
+'''
+'''
+# function call with two values
+def add_number(num1, num2):
+    sum = num1 + num2
+    print("sum:", sum)
+
+# function call with two values
+add_number(5, 4)
+'''
+#the return statement
+# it return a value from the function using the return statement
+#function defination
+'''
+def find_square(num):
+    result = num*num
+    return result
+
+#function call 
+square = find_square(9)
+
+print("square:",square)
+'''

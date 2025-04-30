e51683457338bc7a26798f8774f1d7c0a6071600
vraswate21 <vikasraswate77@gmail.com>
2025-04-30 12:43:31 +0530

    added

---
+++ oops.py
@@ -0,0 +1,16 @@
+#
+
+class ticket:
+    def __init__(self, name,number):
+        self.name = name
+        self.number = number
+
+    def start_point(self):
+        pass
+
+    def End_point(self):
+        pass
+    
+
+a1 = ticket
+    
